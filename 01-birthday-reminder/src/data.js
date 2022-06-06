98aa789976e930fc6361cf620ab27aafabb37808
Florin Nitoiu <nitoiuflorin@gmail.com>
2022-06-06 22:58:45 +0300

    testing

---
+++ 01-birthday-reminder/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Nitoiu Florin",
+    birthday: "02 - 05 - 1995",
+    image:
+      "https://res.cloudinary.com/dmw5wxpu2/image/upload/v1643048558/Demo_zwyoml.jpg",
+  },
+  {
+    id: 2,
+    name: "Pavel  Andreea",
+    birthday: "26 - 11 - 1996",
+    image:
+      "https://res.cloudinary.com/dmw5wxpu2/image/upload/v1643055592/IMG-20211021-WA0012_j943tg.jpg",
+  },
+  {
+    id: 3,
+    name: "Vlad si Rux",
+    birthday: "26",
+    image:
+      "https://res.cloudinary.com/dmw5wxpu2/image/upload/v1643055592/IMG-20211002-WA0003_idhzrm.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    birthday: "34",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    birthday: "29",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
